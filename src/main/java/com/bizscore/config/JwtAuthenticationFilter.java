a51d1fc0b0c3d76de4a99aa676abe7f4e028a356
speculum-factorem <f4gbure@yandex.ru>
2025-12-01 14:04:40 +0300

    Fix ci/cd problems

--- src/main/java/com/bizscore/config/JwtAuthenticationFilter.java
+++ src/main/java/com/bizscore/config/JwtAuthenticationFilter.java
@@ -12,7 +12,6 @@ import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -22,7 +21,6 @@ import java.io.IOException;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-@Order(2) // Порядок выполнения фильтра (после RateLimitFilter)
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private static final String BEARER_PREFIX = "Bearer ";
--- src/main/java/com/bizscore/config/RateLimitFilter.java
+++ src/main/java/com/bizscore/config/RateLimitFilter.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.annotation.Order;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,7 +21,6 @@ import java.io.IOException;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-@Order(1) // Порядок выполнения фильтра (первым, до JWT фильтра)
 public class RateLimitFilter extends OncePerRequestFilter {
 
     private final RateLimitService rateLimitService;
